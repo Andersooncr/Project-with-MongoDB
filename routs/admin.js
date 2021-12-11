5c14a3e9559abe7083583ddef9e4b79b8dbf5569
Andersooncr <andersondocarmo.970@gmail.com>
2021-12-10 22:14:28 -0300

    New commit

--- routs/admin.js
+++ routs/admin.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 require('../models/User')
 const User = mongoose.model("new_user")
+require('../models/Post')
+const Post = mongoose.model("postages")
 
 //Home page
 router.get('/home',(req,res)=>{
@@ -35,27 +37,26 @@ router.post('/register/new',(req,res)=>{
     if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
         crash.push({text: "Invalid name"});
     }
-    if(!req.body.description || typeof req.body.description == undefined || req.body.description == null){
-        crash.push({text: "Invalid description"});
+    if(!req.body.surname || typeof req.body.surname == undefined || req.body.surname == null){
+        crash.push({text: "Invalid surname"});
     }
     if(crash.length>0){
         res.render('admin/register', {crash: crash});
-    }
+    }else{
 
-    //Creat new user
-    const newUser = {
-        name: req.body.name,
-        description: req.body.description
-    }
-    new User(newUser).save().then(()=>{
-        req.flash('success_msg', "User created")
-        res.redirect("/admin/records")
-    }).catch((err)=>{
-        if(crash.length===0){
+        //Creat new user
+        const newUser = {
+            name: req.body.name,
+            surname: req.body.surname
+        }
+        new User(newUser).save().then(()=>{
+            req.flash('success_msg', "User created")
+            res.redirect("/admin/records")
+        }).catch((err)=>{
             req.flash('error_msg', "An error occurred while registering the user")
             res.redirect('/admin/home')
-        }
-    })
+        })
+    }
 });
 
 //Edit page
@@ -75,30 +76,29 @@ router.post('/records/edit',(req,res)=>{
     if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
         crash.push({text: "Invalid name"});
     }
-    if(!req.body.description || typeof req.body.description == undefined || req.body.description == null){
-        crash.push({text: "Invalid description"});
+    if(!req.body.surname || typeof req.body.surname == undefined || req.body.surname == null){
+        crash.push({text: "Invalid surname"});
     }
     if(crash.length>0){
-        res.render('admin/register', {crash: crash});
-    }
+        res.render('admin/records', {crash: crash});
+    }else{
 
-    //Edit user
-    User.findOne({_id:req.body.id}).then((records)=>{
-        records.name = req.body.name
-        records.description = req.body.description
-        records.save().then(()=>{
-            req.flash('success_msg', "User edited")
-            res.redirect("/admin/records")
+        //Edit user
+        User.findOne({_id:req.body.id}).then((records)=>{
+            records.name = req.body.name
+            records.description = req.body.description
+            records.save().then(()=>{
+                req.flash('success_msg', "User edited")
+                res.redirect("/admin/records")
+            }).catch((err)=>{
+                req.flash('error_msg', "An error occurred while updating the user")
+                res.redirect('/admin/records')
+            })
         }).catch((err)=>{
             req.flash('error_msg', "An error occurred while updating the user")
             res.redirect('/admin/records')
         })
-    }).catch((err)=>{
-        if(crash.length===0){
-            req.flash('error_msg', "An error occurred while updating the user")
-            res.redirect('/admin/records')
-        }
-    })
+    }
 })
 
 //Delete user
@@ -117,14 +117,50 @@ router.get('/posts',(req,res)=>{
     res.render("admin/posts")
 })
 
-//Register post post
-router.get('/posts/new',(req,res)=>{
+//Register post
+router.get('/new-post',(req,res)=>{
     User.find().lean().then((records)=>{
         res.render("admin/new_post", {records: records})
     }).catch((err)=>{
         req.flash("error_msg", "An error ocurred while load form")
         res.redirect('/admin')
     })
+})
+
+//Create new post
+router.post('/posts/new',(req,res)=>{
+    //validation 
+    if(!req.body.alert || typeof req.body.alert == undefined || req.body.alert == null){
+        crash.push({text: "Invalid alert"});
+    }
+    if(!req.body.description || typeof req.body.description == undefined || req.body.description == null){
+        crash.push({text: "Invalid description"});
+    }
+    if(!req.body.contents || typeof req.body.contents == undefined || req.body.contents == null){
+        crash.push({text: "Invalid contents"});
+    }
+    if(req.body.user == "0"){
+        crash.push({text: "Invalid user, register a user"});
+    }
+    if(crash.length>0){
+        res.render('admin/new_post', {crash: crash});
+    }else{
+
+        //New post
+        const newPost = {
+            alert: req.body.alert,
+            description: req.body.description,
+            contents: req.body.contents,
+            user: req.body.user
+        }
+        new Post(newPost).save().then(()=>{
+            req.flash('success_msg', "Post created")
+            res.redirect("/admin/posts")
+        }).catch((err)=>{
+            req.flash('error_msg', "An error ocurred while register the post")
+            res.redirect('/admin/home')
+        })
+    }
 })
 
 module.exports = router
